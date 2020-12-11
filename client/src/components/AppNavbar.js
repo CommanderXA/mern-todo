ad5e06c6d29e7ad945800866dfc5ad07244f3b5c
CommanderXA <53992051+CommanderXA@users.noreply.github.com>
2020-12-11 21:29:06 +0600

    Backend & frontend connected

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://google.com">Google</NavLink>
+                                    <NavLink target="_black" href="https://github.com/CommanderXA/mern-todo">GitRepo</NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
@@ -6,16 +5,18 @@ const initialState = {
     loading: false
 }
 
-export default function(state = initialState, action) {
+export default function reducer(state = initialState, action) {
     switch(action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
             };
         case ADD_ITEM:
             return {
